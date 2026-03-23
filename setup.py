0c0b8c3ff236678ffd8fbf241cb2ab3ba840a917
Liam <liam.folsviks@gmail.com>
2026-03-23 14:52:17 +0100

    Fixed the faulty ROS integration, created launch file, integrated publisher for buoys and boats

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import find_packages, setup
-
-package_name = 'object_detector'
+import os
+from glob import glob
+package_name = 'usv_object_detector'
 
 setup(
     name=package_name,
@@ -10,6 +11,8 @@ setup(
         ('share/ament_index/resource_index/packages', 
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share','usv_object_detector','models'),glob('models/*')),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,7 +27,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'object_detector = object_detector.ObjectDetectionNode:main'
+            'usv_object_detector = usv_object_detector.ObjectDetectionNode:main'
         ],
     },
 )
